0731ef248a61b19fb51984a9b75d528ff9a8b0d5
Constantin <ktimonin4@gmail.com>
2024-05-23 14:39:35 +0400

    Create project

---
+++ src/main/java/ru/yandex/practicum/catsgram/exceptions/ConditionsNotMetException.java
@@ -0,0 +1,10 @@
+package ru.yandex.practicum.catsgram.exceptions;
+
+public class ConditionsNotMetException extends RuntimeException {
+    public ConditionsNotMetException(String message) {
+        super(message);
+    }
+    public ConditionsNotMetException() {
+        super();
+    }
+}
---
+++ src/main/java/ru/yandex/practicum/catsgram/exceptions/NotFoundException.java
@@ -0,0 +1,10 @@
+package ru.yandex.practicum.catsgram.exceptions;
+
+public class NotFoundException extends RuntimeException{
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+    public NotFoundException() {
+    }
+}
